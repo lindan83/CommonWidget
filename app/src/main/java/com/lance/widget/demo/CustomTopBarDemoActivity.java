8a6636ce3b5b124f2493b1a65a18cf39a6851b98
lindan <jackcoor@hotmail.com>
2016-11-01 11:18:12 +0800

    fix some bugs

--- app/src/main/java/com/lance/widget/demo/CustomTopBarDemoActivity.java
+++ app/src/main/java/com/lance/widget/demo/CustomTopBarDemoActivity.java
@@ -1,7 +1,7 @@
 package com.lance.widget.demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.lance.common.util.ToastUtil;
 import com.lance.common.widget.TopBar;
