5d89cd4a33ec8f6fdbb90d769204cfa053a4ce4e
lindan <jackcoor@hotmail.com>
2016-11-01 10:51:30 +0800

    fix some bugs

--- app/src/main/java/com/lance/widget/demo/CommonTopBarDemoActivity.java
+++ app/src/main/java/com/lance/widget/demo/CommonTopBarDemoActivity.java
@@ -1,72 +1,38 @@
 package com.lance.widget.demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.lance.common.util.ToastUtil;
 import com.lance.common.widget.TopBar;
 
 public class CommonTopBarDemoActivity extends AppCompatActivity {
-    private TopBar mTopBarView1, mTopBarView2, mTopBarView3;
+    private TopBar mTopBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_common_top_bar_demo);
 
-        mTopBarView1 = (TopBar) findViewById(R.id.top_bar_1);
-        mTopBarView2 = (TopBar) findViewById(R.id.top_bar_2);
-        mTopBarView3 = (TopBar) findViewById(R.id.top_bar_3);
+        mTopBar = (TopBar) findViewById(R.id.top_bar);
 
-        mTopBarView1.setOnClickTopBarListener(new TopBar.OnClickListener() {
-            @Override
-            public void onClickLeft() {
-                ToastUtil.showShort(CommonTopBarDemoActivity.this, "click left");
-            }
+        if (mTopBar != null) {
+            mTopBar.setOnClickTopBarListener(new TopBar.OnClickListener() {
+                @Override
+                public void onClickLeft() {
+                    ToastUtil.showShort(CommonTopBarDemoActivity.this, "click left");
+                }
 
-            @Override
-            public void onClickTitle() {
-                ToastUtil.showShort(CommonTopBarDemoActivity.this, "click title");
-            }
+                @Override
+                public void onClickTitle() {
+                    ToastUtil.showShort(CommonTopBarDemoActivity.this, "click title");
+                }
 
-            @Override
-            public void onClickRight() {
-                ToastUtil.showShort(CommonTopBarDemoActivity.this, "click right");
-            }
-        });
-
-        mTopBarView2.setOnClickTopBarListener(new TopBar.OnClickListener() {
-            @Override
-            public void onClickLeft() {
-                ToastUtil.showShort(CommonTopBarDemoActivity.this, "click left");
-            }
-
-            @Override
-            public void onClickTitle() {
-                ToastUtil.showShort(CommonTopBarDemoActivity.this, "click title");
-            }
-
-            @Override
-            public void onClickRight() {
-                ToastUtil.showShort(CommonTopBarDemoActivity.this, "click right");
-            }
-        });
-
-        mTopBarView3.setOnClickTopBarListener(new TopBar.OnClickListener() {
-            @Override
-            public void onClickLeft() {
-                ToastUtil.showShort(CommonTopBarDemoActivity.this, "click left");
-            }
-
-            @Override
-            public void onClickTitle() {
-                ToastUtil.showShort(CommonTopBarDemoActivity.this, "click title");
-            }
-
-            @Override
-            public void onClickRight() {
-                ToastUtil.showShort(CommonTopBarDemoActivity.this, "click right");
-            }
-        });
+                @Override
+                public void onClickRight() {
+                    ToastUtil.showShort(CommonTopBarDemoActivity.this, "click right");
+                }
+            });
+        }
     }
 }
